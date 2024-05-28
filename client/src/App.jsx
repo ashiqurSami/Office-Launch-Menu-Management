50dc1fe7516438cb88bce83e710697436e683973
ashiqursami <2020741@iub.edu.bd>
2024-05-29 01:49:14 +0600

    feat: add menu creation UI and functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Registration from "./pages/registration_page";
 import { Toaster } from 'react-hot-toast';
 import LoginPage from "./pages/login_page";
+import Admin from "./pages/admin_page";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Registration />} />
         <Route path='/login' element={<LoginPage />} />
+        <Route path='/admin' element={<Admin></Admin>} />
       </Routes>
       <Toaster />
     </BrowserRouter>
