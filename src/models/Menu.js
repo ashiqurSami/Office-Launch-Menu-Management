864c6f94c60c665c2e7a3883fdbc9c10a6cb4302
ashiqursami <2020741@iub.edu.bd>
2024-05-30 04:45:15 +0600

    fix: modify server name

--- src/models/Menu.js
+++ src/models/Menu.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const MenuSchema = new mongoose.Schema({
   date: {
@@ -15,7 +15,7 @@ const MenuSchema = new mongoose.Schema({
     {
       userId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
+        ref: "User",
       },
       option: {
         type: String,
@@ -25,4 +25,4 @@ const MenuSchema = new mongoose.Schema({
   ],
 });
 
-module.exports = mongoose.model('Menu', MenuSchema);
+module.exports = mongoose.model("Menu", MenuSchema);
